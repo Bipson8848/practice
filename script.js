e878ade97e71748e0b524e8b0b7dfc04b1bda037
unknown <bipson490@gmail.com>
2025-07-05 22:22:11 +0545

    done all the logical things

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-let rand=Math.floor(Math.random()*3+1);
 let score=document.querySelector('.score');
+let user=document.querySelector('.user-guess');
+let arr=['scissor','paper','rock'];
+
+user.addEventListener('click',function(element){
+    let rand=Math.floor(Math.random()*3+1);
+    let index=element.target.parentElement.dataset.value;
+    generateResult(index,rand);
+    console.log(rand);  
+})
+
 function generateResult(userguess,rand){
     if(userguess==rand){
         score.innerHTML="Draw";
+    }else if((userguess==0 && rand==2)){
+        score.innerHTML=`You wonnnn computer chooses ${arr[rand]}`;
+    }else if((userguess==2 && rand==0)){
+        score.innerHTML=`You loose computer chooses ${arr[rand]}`;
     }
+    else if(rand>userguess){
+        score.innerHTML=`you loseee :( computer chooses ${arr[rand]}`;
+    }else{
+        score.innerHTML=`you wonnnn :)))) computer chooses ${arr[rand]}`;
 
+    }
 }
