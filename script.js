172ec57d341e88a8dd97c6ce2b44206243a40be1
unknown <bipson490@gmail.com>
2025-07-05 22:56:53 +0545

    adding

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 let score=document.querySelector('.score');
 let user=document.querySelector('.user-guess');
-let arr=['scissor','paper','rock'];
+let arr=['rock','paper','scissors'];
 
 user.addEventListener('click',function(element){
-    let rand=Math.floor(Math.random()*3+1);
+    let rand=Math.floor(Math.random()*3);
     let index=element.target.parentElement.dataset.value;
+    console.log(index)
     generateResult(index,rand);
-    console.log(rand);  
 })
 
 function generateResult(userguess,rand){
+    console.log(userguess,rand);
     if(userguess==rand){
-        score.innerHTML="Draw";
+        score.innerHTML="Drawwwwwwwwwwwwwwwwwwwwwwwwww";
     }else if((userguess==0 && rand==2)){
         score.innerHTML=`You wonnnn computer chooses ${arr[rand]}`;
     }else if((userguess==2 && rand==0)){
